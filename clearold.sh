7e71965966e03a8e563af003ae1c1f9b8050a6ba
manij-20 <jmanikanta321@gmail.com>
2026-03-22 13:00:39 +0530

    Update clearold.sh

--- clearold.sh
+++ clearold.sh
@@ -6,4 +6,27 @@ read count
 
 count=$(( total - count ))
 
-head -$count clean
+head -$count clean | xargs rm -f
+---------------------------------
+
+
+
+ls -lrt
+-l → long listing (details)
+-r → reverse order
+-t → sort by time
+👉 Final result: oldest files come first, newest last
+awk -F " " 'NR>1{print $NF}'
+NR>1 → skip first line (total)
+$NF → print last field → file name
+> clean
+Saves all filenames into a file named clean
+
+cat clean → read file
+wc -l → count lines (i.e., number of files)
+total = total number of files
+
+
+Converts "files to retain" → "files to delete"
+
+head -$count clean 
